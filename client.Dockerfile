1bd84cd407ca64aa112db6a64f958630061b61e0
Александр <karavanov.ad1@gmail.com>
2022-04-05 16:25:28 +0300

    Revert "Initial commit"
    This reverts commit f1e94e8aa21e0ed7f80096ca8f72fbc2de27b56c.

--- client.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:11
-ARG JAR_FILE=client/target/client-0.0.1-SNAPSHOT.jar
-ADD ${JAR_FILE} client.jar
-EXPOSE 8411
-ENTRYPOINT ["java","-jar","/client.jar"]
--- server/src/main/java/com/example/server/repo/ServerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.repo;
-
-import com.example.server.entity.Server;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServerRepository extends CrudRepository<Server, Long> {
-}
